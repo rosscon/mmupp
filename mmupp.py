aa2394500b250e0c90269096e6aa696d45fd7670
Ross Conroy <9072796+rosscon@users.noreply.github.com>
2022-06-02 21:42:31 +0100

    updated README and added reset keys

--- mmupp.py
+++ mmupp.py
@@ -11,8 +11,20 @@ Config keys
 CONFIG_KEY_DIP_DISTANCE      = ';mmupp:dip_distance'
 CONFIG_KEY_DIP_INSERT_SPEED  = ';mmupp:dip_insert_speed'
 CONFIG_KEY_DIP_RETRACT_SPEED = ';mmupp:dip_retract_speed'
+CONFIG_KEY_DIP_POST_PAUSE    = ';mmupp:dip_post_pause'
 
 CONFIG_KEY_RAMMING_TEMP      = ';mmupp:ramming_temp'
+
+
+"""
+Config reset keys
+"""
+CONFIG_KEY_DIP_DISTANCE_RST      = ';mmupp:rst:dip_distance'
+CONFIG_KEY_DIP_INSERT_SPEED_RST  = ';mmupp:rst:dip_insert_speed'
+CONFIG_KEY_DIP_RETRACT_SPEED_RST = ';mmupp:rst:dip_retract_speed'
+CONFIG_KEY_DIP_POST_PAUSE_RST    = ';mmupp:rst:dip_post_pause'
+
+CONFIG_KEY_RAMMING_TEMP_RST      = ';mmupp:rst:ramming_temp'
 
 
 """
@@ -21,8 +33,17 @@ Values
 DIP_DISTANCE = -1
 DIP_INSERT_SPEED = 2000
 DIP_RETRACT_SPEED = 4000
+DIP_POST_PAUSE = 8000
 
 RAMMING_TEMP = -1
+
+
+DIP_DISTANCE_PREV = -1
+DIP_INSERT_SPEED_PREV = 2000
+DIP_RETRACT_SPEED_PREV = 4000
+DIP_POST_PAUSE_PREV = 8000
+
+RAMMING_TEMP_PREV = -1
 
 
 """
@@ -58,13 +79,43 @@ for i in range(len(lines)):
     Check for any config changes in line
     """
     if line.startswith(CONFIG_KEY_DIP_DISTANCE):
+        DIP_DISTANCE_PREV = DIP_DISTANCE
         DIP_DISTANCE = float(line.split('=')[1])
+    
     elif line.startswith(CONFIG_KEY_DIP_INSERT_SPEED):
+        DIP_INSERT_SPEED_PREV = DIP_INSERT_SPEED
         DIP_INSERT_SPEED = float(line.split('=')[1])
+   
     elif line.startswith(CONFIG_KEY_DIP_RETRACT_SPEED):
+        DIP_RETRACT_SPEED_PREV = DIP_RETRACT_SPEED
         DIP_RETRACT_SPEED = float(line.split('=')[1])
+    
+    elif line.startswith(CONFIG_KEY_DIP_POST_PAUSE):
+        DIP_POST_PAUSE_PREV = DIP_POST_PAUSE
+        DIP_POST_PAUSE = int(line.split('=')[1])
+    
     elif line.startswith(CONFIG_KEY_RAMMING_TEMP):
+        RAMMING_TEMP_PREV = RAMMING_TEMP
         RAMMING_TEMP = float(line.split('=')[1])
+        
+        
+    """
+    Check for any config resets
+    """
+    if line.startswith(CONFIG_KEY_DIP_DISTANCE_RST):
+        DIP_DISTANCE = DIP_DISTANCE_PREV
+    
+    elif line.startswith(CONFIG_KEY_DIP_INSERT_SPEED_RST):
+        DIP_INSERT_SPEED = DIP_INSERT_SPEED_PREV
+   
+    elif line.startswith(CONFIG_KEY_DIP_RETRACT_SPEED_RST):
+        DIP_RETRACT_SPEED = DIP_RETRACT_SPEED_PREV
+    
+    elif line.startswith(CONFIG_KEY_DIP_POST_PAUSE_RST):
+        DIP_POST_PAUSE = DIP_POST_PAUSE_PREV
+    
+    elif line.startswith(CONFIG_KEY_RAMMING_TEMP_RST):
+        RAMMING_TEMP = RAMMING_TEMP_PREV
     
 
 
@@ -79,13 +130,24 @@ for i in range(len(lines)):
         # an extract followed immediatly by a retract
         if re.match(r"G1.*E-[0-9]", lines[i-1]) and re.match(r"G1.*E[0-9]", lines[i-2]):
             
-            if (DIP_DISTANCE > 0):
+            if DIP_DISTANCE > 0:
                 # Add dip
                 dip_insert = "G1 E" + str(DIP_DISTANCE) + " F" + str(DIP_INSERT_SPEED)
                 dip_retract = "G1 E-" + str(DIP_DISTANCE) + " F" + str(DIP_RETRACT_SPEED)
                 new_lines.append(";MMUPP: DIP ADDED")
                 new_lines.append(dip_insert)
                 new_lines.append(dip_retract)
+                
+                # Repeat last cooling moves
+                new_lines.append(lines[i-2])
+                new_lines.append(lines[i-1])
+                new_lines.append(lines[i-2])
+                new_lines.append(lines[i-1])
+                new_lines.append(lines[i-2])
+                new_lines.append(lines[i-1])
+                
+                if DIP_POST_PAUSE > 0:
+                    new_lines.append("G4 P" + str(DIP_POST_PAUSE))
                 
                 
                 
